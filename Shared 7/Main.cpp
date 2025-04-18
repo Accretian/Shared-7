30bc2e037865d55c289bbbf9d3b7b4651e050019
Accretian <kvik.ocean@gmail.com>
2025-04-18 15:06:07 +0300

    1

--- Shared 7/Main.cpp
+++ Shared 7/Main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main() {
 
 	std::string str;
+	std::vector<std::string> vin;
+	std::vector<std::string> vout;
 
-
-
-
-
-
+	while (true) {
+		std::getline(std::cin, str);
+		if (str != "")
+			vin.push_back(str);
+		else break;
+	}
 
 
 
