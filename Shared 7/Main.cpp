68d0b8f0850623312f9fd32b100678201d4d7c3f
Accretian <kvik.ocean@gmail.com>
2025-04-18 15:43:10 +0300

    1

--- Shared 7/Main.cpp
+++ Shared 7/Main.cpp
@@ -3,11 +3,38 @@
 #include <string>
 
 
-std::vector<std::string> vectorrecursion(std::vector<std::string> vector) {
+std::vector<std::string> vectorrecursion(std::vector<std::string> vector, int numberofch) {
+	int numberofactions = 0;
+	std::vector<std::string> v2;
+	std::copy(vector.begin(), vector.end(), v2);
+
+	for (int i = 0; i != (int)v2.size(); i++) {
+		while (true) {
+			for (std::string s : v2) {
+				if (numberofch < (int)s.size()) {
+					if ()
+				}
+			}
+		}
+	}
 
 
 
 
+
+
+	for (std::string str : v2) {
+		if (numberofch < (int)str.size()) {
+			for (int i = 0; i != (int)v2.size(); i++) {
+				if (v2[i])
+			}
+
+		}
+	}
+	
+
+
+	
 
 
 }
